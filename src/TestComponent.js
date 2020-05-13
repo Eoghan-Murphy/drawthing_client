578dacb4b4b0fdec415c5d90e54e341486ee4e70
Eoghan <eoghanmurphycode@gmail.com>
2020-05-13 13:54:01 +0100

    Initial react client, with test component

---
+++ src/TestComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Row} from 'reactstrap';
+
+class TestComponent extends React.Component {
+    render () {
+        return(
+            <Container className="h-100">
+                <Row>
+                    <p>TEST</p>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+  
+export default TestComponent;
